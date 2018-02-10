8f72250eeca786eb56c8861ea4f77f2595244eb1
Spencer Kim <sabin1997@gmail.com>
2018-02-10 10:34:27 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /.*\.png$/i,
+        loaders: [ 'file-loader', {
+          loader: 'image-webpack-loader',
+          query: {
+            progressive: true,
+            pngquant: {
+              quality: '65-90',
+              speed: 4
+            }
+          }
+        }]
+      }
+    ]
+  },
+  plugins: [HtmlWebpackPluginConfig],
+};
